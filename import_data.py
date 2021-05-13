6da3b7668708080419742af7874d90413cb29266
Benjamin Kleeman <benjamin.kleeman@gmail.com>
2021-05-13 13:01:44 -0500

    add comments for useful parts of atlast-db ingestors

--- import_data.py
+++ import_data.py
@@ -26,12 +26,42 @@ def append_props_obj(file_data):
 # append props to each file in the directory and insert each into the collection
 def import_files():
     for filename in os.listdir('./'):
-        with open(filename) as file:
-            file_data = json.load(file)
-            append_props_obj(file_data)
+        if filename != 'import_data.py':
+            with open(filename) as file:
+                file_data = json.load(file)
+                append_props_obj(file_data)
 
-            if isinstance(file_data, list):
-                Collection.insert_many(file_data)
-            else: Collection.insert_one(file_data)
+                if isinstance(file_data, list):
+                    Collection.insert_many(file_data)
+                else: Collection.insert_one(file_data)
 
 import_files()
+
+# We want to use create index
+
+# declare schema for all records as a python object -- check line 160 of atlas_db ingestors mongodb.py file
+# See the init file for where the AtlasSchema class is defined
+
+# class AtlasMongoDocument(AtlasSchema):
+#     def __init__(self, *args, **kwargs):
+#         """Schema for storing ATLAS data in Mongo.
+#         """
+#         super(AtlasMongoDocument, self).__init__(*args, **kwargs)
+
+#     @property
+#     def __geo_interface__(self):
+#         """Define centroid (x, y) as a GeoJSON point. n-d array of values
+#          in the `properties` attribute.
+#         :return: GeoJSON object representing data point
+#         :rtype: dict
+#         """
+
+#         document = {
+#             # 'type': 'Feature',
+#             'geometry': {'type': 'Point',
+#                          'coordinates': [self.x, self.y]},
+#             'properties': {
+#                 'values': self.value,
+#             }}
+
+#         return document
